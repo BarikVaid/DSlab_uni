6c27691fe189d6a278805074aa89f5a6331ff9fd
Bar <barbloggeron@gmail.com>
2025-10-11 21:13:57 +0530

    update

--- Arrays/AccessElements.c
+++ Arrays/AccessElements.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    int arr[50], n, i, pos;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("\nArray elements: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    printf("\nEnter position to access (1 to %d): ", n);
+    scanf("%d", &pos);
+    
+    if(pos < 1 || pos > n) {
+        printf("Invalid position!\n");
+        return 0;
+    }
+    
+    printf("Element at position %d: %d\n", pos, arr[pos-1]);
+    
+    return 0;
+}
