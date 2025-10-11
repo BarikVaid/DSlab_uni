49f161ce481ccf11da8ea0be94a11ae04c31462b
Bar <barbloggeron@gmail.com>
2025-10-12 00:19:02 +0530

    Add files via upload

---
+++ String/DeleteBeginning.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        str[i] = str[i + 1];
+    }
+
+    printf("After deleting first character: %s\n", str);
+    return 0;
+}
+//cse78
---
+++ String/StringCreation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("The string is: %s\n", str);
+    return 0;
+}
+//cse78
