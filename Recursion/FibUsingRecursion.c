cc2218611400d345ae2f7fec8163f30f9a0d6a46
Bar <barbloggeron@gmail.com>
2025-10-12 20:34:51 +0530

    Edit

---
+++ Recursion/FibUsingRecursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int fib(int n)
+{
+    if(n==0)
+        return 0;
+    else if (n==1)
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+    int t,i;
+    printf("Enter the number of terms you want:");
+    scanf("%d", &t);
+    printf("\nFibonacci Series: ");
+    for(i=0;i<t;i++)
+    {
+        printf("%d ",fib(i));
+    }
+    return 0;
+}
